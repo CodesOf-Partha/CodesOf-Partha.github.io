3e718377e6622ac4b8db9fed81b00b59ac1d1777
p0rth01998 <rvparthasarathi@gmail.com>
2020-08-08 17:13:30 +0530

    Updates

---
+++ precache-manifest.b55aee3a43dc30020640a96b5a34d803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb3ac15a3be891a9ec27deb1e74184a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d093b5a5449d120d55",
+    "url": "/static/css/main.df81b4da.chunk.css"
+  },
+  {
+    "revision": "39392e7ec36cf254f7e7",
+    "url": "/static/js/2.e9623ec0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e9623ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d093b5a5449d120d55",
+    "url": "/static/js/main.23a1d5c4.chunk.js"
+  },
+  {
+    "revision": "1033504dd10034a7501f",
+    "url": "/static/js/runtime-main.f92c86ac.js"
+  },
+  {
+    "revision": "ef035e286d3afbac94cab6dbd5842b20",
+    "url": "/static/media/icon.ef035e28.png"
+  }
+]);
